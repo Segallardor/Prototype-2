913fcb4800b96f797a03b04bccf5c4b63546f829
Segallardor <121991848+Segallardor@users.noreply.github.com>
2023-02-10 15:05:27 -0800

    Unit 2.2 8start

--- Assets/Scripts/DestroyOutOfBounds.cs
+++ Assets/Scripts/DestroyOutOfBounds.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class DestroyOutOfBounds : MonoBehaviour
 {
     private float topBound = 20;
+    private float lowerbound = -10;
     // Start is called before the first frame update
     void Start()
     {
@@ -16,6 +17,8 @@ public class DestroyOutOfBounds : MonoBehaviour
     {
         if (transform.position.z > topBound)
         {
+            Destroy(gameObject);
+        }   else if (transform.position.z < lowerbound) {
             Destroy(gameObject);
         }
     }
